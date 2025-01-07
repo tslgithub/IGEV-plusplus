5e5f2b4f2242505881113ebb7b07f8fdea147f4a
tslgithub <tiansonglu@gmail.com>
2025-01-07 15:45:12 +0800

    Update demo_imgs.py

--- demo_imgs.py
+++ demo_imgs.py
@@ -22,9 +22,13 @@ DEVICE = 'cuda'
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 def load_image(imfile):
+    
     img = np.array(Image.open(imfile)).astype(np.uint8)[..., :3]
     img = torch.from_numpy(img).permute(2, 0, 1).float()
     return img[None].to(DEVICE)
+
+
+
 
 def demo(args):
     model = torch.nn.DataParallel(IGEVStereo(args), device_ids=[0])
@@ -43,22 +47,42 @@ def demo(args):
         print(f"Found {len(left_images)} images. Saving files to {output_directory}/")
 
         for (imfile1, imfile2) in tqdm(list(zip(left_images, right_images))):
+            left_cv_image = cv2.imread(imfile1)
+            right_cv_image = cv2.imread(imfile1)
+
             image1 = load_image(imfile1)
             image2 = load_image(imfile2)
             padder = InputPadder(image1.shape, divis_by=32)
             image1, image2 = padder.pad(image1, image2)
             disp = model(image1, image2, iters=args.valid_iters, test_mode=True)
             disp = padder.unpad(disp)
-            file_stem = imfile1.split('/')[-2]
-            filename = os.path.join(output_directory, f'{file_stem}.png')
+            file_stem = imfile1.split('/')[-1]
+            filename = os.path.join(output_directory, file_stem.split(".")[0]+"_depth.png" )
+            # print("imfile1 ->{}".format(imfile1))
             disp = disp.cpu().numpy().squeeze()
-            plt.imsave(filename, disp.squeeze(), cmap='jet')
+            # plt.imsave(filename, disp.squeeze(), cmap='jet')
             
             if args.save_numpy:
                 np.save(output_directory / f"{file_stem}.npy", disp.squeeze())
 
-            # disp = np.round(disp * 256).astype(np.uint16)
-            # cv2.imwrite(filename, cv2.applyColorMap(cv2.convertScaleAbs(disp.squeeze(), alpha=0.01),cv2.COLORMAP_JET), [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+            disp = np.round(disp * 256).astype(np.uint16)
+            depth = cv2.applyColorMap(cv2.convertScaleAbs(disp.squeeze(), alpha=0.01),cv2.COLORMAP_JET)
+            depth = np.append(np.append(left_cv_image,right_cv_image,1),depth,1)
+
+            cv2.rectangle(depth,(0,0),  (160,70),(0,255,0),-1)
+            cv2.rectangle(depth,(640,0),(640+200,70),(0,255,0),-1)
+            cv2.rectangle(depth,(640+640,0),(640+640+210,70),(0,255,0),-1)
+            cv2.putText(depth,"left",  (20, 50), 5,3, (255,0,255),3)
+            cv2.putText(depth,"right", (0+640,50),5,3,(255,0,255),3)
+            cv2.putText(depth,"depth", (0+640+640,50),5,3,(255,0,255),3)
+            ih,iw,ic = depth.shape
+            cv2.line(depth,(int(iw/3),0),(int(iw/3),ih),(255,0,128),4,1 )
+            cv2.line(depth,(int(iw/3)*2,0),(int(iw/3)*2,ih),(255,0,128),4,2 )
+
+            cv2.imwrite(filename,depth , [int(cv2.IMWRITE_PNG_COMPRESSION), 0] )
+
+            cv2.imshow("depth",depth)
+            cv2.waitKey(1)
 
 
 if __name__ == '__main__':
@@ -71,6 +95,7 @@ if __name__ == '__main__':
     parser.add_argument('--mixed_precision', action='store_true', default=True, help='use mixed precision')
     parser.add_argument('--precision_dtype', default='float32', choices=['float16', 'bfloat16', 'float32'], help='Choose precision type: float16 or bfloat16 or float32')
     parser.add_argument('--valid_iters', type=int, default=16, help='number of flow-field updates during forward pass')
+    parser.add_argument('--save_path', type=str, default="./demo_output", help='save result path')
 
     # Architecture choices
     parser.add_argument('--hidden_dims', nargs='+', type=int, default=[128]*3, help="hidden state and context dimensions")
